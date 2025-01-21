945275481e49daa6049c8d8a09916cc09a96000a
Tomcy Thomas <tomcy48@gmail.com>
2025-01-21 18:56:37 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# NextJS + Bun
+
+NextJS with the flavour of Bun and BiomeJS.
+
+## Getting Started
+
+First, run the development server:
+
+```bash
+bun run dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+
+You can start editing the page by modifying `src/app/page.tsx`. The page auto-updates as you edit the file.
---
+++ src/components/ui/label.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+// biome-ignore lint/style/noNamespaceImport: <explanation>
+import * as LabelPrimitive from "@radix-ui/react-label";
+import { type VariantProps, cva } from "class-variance-authority";
+import React from "react";
+
+import { cn } from "@/lib/utils";
+
+const labelVariants = cva(
+	"font-medium text-sm leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
+);
+
+const Label = React.forwardRef<
+	React.ElementRef<typeof LabelPrimitive.Root>,
+	React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> &
+		VariantProps<typeof labelVariants>
+>(({ className, ...props }, ref) => (
+	<LabelPrimitive.Root
+		ref={ref}
+		className={cn(labelVariants(), className)}
+		{...props}
+	/>
+));
+Label.displayName = LabelPrimitive.Root.displayName;
+
+export { Label };
